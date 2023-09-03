207b3dddb99872d033f332511218955f4ceec82f
bonce <chris@headstrong.solutions>
2023-09-03 14:51:54 +0100

    added a sleep

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-cd /source/jarvis_printer/
+cd /home/bonce/source/jarvis_printer/
 
-. venv/bin/activate
-gunicorn -w 1 -b 0.0.0.0 app:app -D --log-file=gunicorn.log
+#. venv/bin/activate removed as worried about venv stealing memory
+#gunicorn -w 1 -b 0.0.0.0:4000 app:app -D --log-file=gunicorn.log
+gunicorn -w 1 -b 0.0.0.0:4000 app:app -D
 
+sleep 5
